45d828ae8642bfb36df29f19d7a1676cf99beded
rysonw <rysonplays@gmail.com>
2022-10-17 05:14:09 -1000

    First Attempt at implementation.

--- FacialRecognition/Form1.Designer.cs
+++ FacialRecognition/Form1.Designer.cs
@@ -28,6 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.picCapture = new System.Windows.Forms.PictureBox();
             this.DetectButton = new System.Windows.Forms.Button();
             this.textPersonName = new System.Windows.Forms.TextBox();
@@ -35,22 +38,34 @@
             this.TrainButton = new System.Windows.Forms.Button();
             this.recognizeButton = new System.Windows.Forms.Button();
             this.SamplePictureBox = new System.Windows.Forms.PictureBox();
-            this.SaveButton = new System.Windows.Forms.Button();
+            this.SaveAndSendButton = new System.Windows.Forms.Button();
+            this.moodChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.confidenceLabel = new System.Windows.Forms.Label();
+            this.supriseLabel = new System.Windows.Forms.Label();
+            this.angerLabel = new System.Windows.Forms.Label();
+            this.sorrowLabel = new System.Windows.Forms.Label();
+            this.joyLabel = new System.Windows.Forms.Label();
+            this.surpriseOutput = new System.Windows.Forms.TextBox();
+            this.confidenceOutput = new System.Windows.Forms.TextBox();
+            this.joyOutput = new System.Windows.Forms.TextBox();
+            this.sorrowOutput = new System.Windows.Forms.TextBox();
+            this.angerOutput = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.picCapture)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.SamplePictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moodChart)).BeginInit();
             this.SuspendLayout();
             // 
             // picCapture
             // 
             this.picCapture.Location = new System.Drawing.Point(10, 10);
             this.picCapture.Name = "picCapture";
-            this.picCapture.Size = new System.Drawing.Size(634, 432);
+            this.picCapture.Size = new System.Drawing.Size(634, 510);
             this.picCapture.TabIndex = 0;
             this.picCapture.TabStop = false;
             // 
             // DetectButton
             // 
-            this.DetectButton.Location = new System.Drawing.Point(659, 49);
+            this.DetectButton.Location = new System.Drawing.Point(659, 12);
             this.DetectButton.Name = "DetectButton";
             this.DetectButton.Size = new System.Drawing.Size(127, 27);
             this.DetectButton.TabIndex = 2;
@@ -60,14 +75,14 @@
             // 
             // textPersonName
             // 
-            this.textPersonName.Location = new System.Drawing.Point(659, 261);
+            this.textPersonName.Location = new System.Drawing.Point(659, 197);
             this.textPersonName.Name = "textPersonName";
             this.textPersonName.Size = new System.Drawing.Size(127, 20);
             this.textPersonName.TabIndex = 3;
             // 
             // AddPersonButton
             // 
-            this.AddPersonButton.Location = new System.Drawing.Point(659, 82);
+            this.AddPersonButton.Location = new System.Drawing.Point(659, 45);
             this.AddPersonButton.Name = "AddPersonButton";
             this.AddPersonButton.Size = new System.Drawing.Size(127, 23);
             this.AddPersonButton.TabIndex = 4;
@@ -77,7 +92,7 @@
             // 
             // TrainButton
             // 
-            this.TrainButton.Location = new System.Drawing.Point(659, 339);
+            this.TrainButton.Location = new System.Drawing.Point(659, 255);
             this.TrainButton.Name = "TrainButton";
             this.TrainButton.Size = new System.Drawing.Size(127, 26);
             this.TrainButton.TabIndex = 5;
@@ -87,7 +102,7 @@
             // 
             // recognizeButton
             // 
-            this.recognizeButton.Location = new System.Drawing.Point(659, 369);
+            this.recognizeButton.Location = new System.Drawing.Point(659, 287);
             this.recognizeButton.Name = "recognizeButton";
             this.recognizeButton.Size = new System.Drawing.Size(127, 25);
             this.recognizeButton.TabIndex = 6;
@@ -96,28 +111,141 @@
             // 
             // SamplePictureBox
             // 
-            this.SamplePictureBox.Location = new System.Drawing.Point(659, 138);
+            this.SamplePictureBox.Location = new System.Drawing.Point(659, 74);
             this.SamplePictureBox.Name = "SamplePictureBox";
             this.SamplePictureBox.Size = new System.Drawing.Size(127, 117);
             this.SamplePictureBox.TabIndex = 7;
             this.SamplePictureBox.TabStop = false;
             // 
-            // SaveButton
+            // SaveAndSendButton
             // 
-            this.SaveButton.Location = new System.Drawing.Point(659, 310);
-            this.SaveButton.Name = "SaveButton";
-            this.SaveButton.Size = new System.Drawing.Size(127, 25);
-            this.SaveButton.TabIndex = 8;
-            this.SaveButton.Text = "Save and Send to API";
-            this.SaveButton.UseVisualStyleBackColor = true;
-            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            this.SaveAndSendButton.Location = new System.Drawing.Point(659, 224);
+            this.SaveAndSendButton.Name = "SaveAndSendButton";
+            this.SaveAndSendButton.Size = new System.Drawing.Size(127, 25);
+            this.SaveAndSendButton.TabIndex = 8;
+            this.SaveAndSendButton.Text = "Save and Evaluate";
+            this.SaveAndSendButton.UseVisualStyleBackColor = true;
+            this.SaveAndSendButton.Click += new System.EventHandler(this.SaveAndSendButton_Click);
+            // 
+            // moodChart
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.moodChart.ChartAreas.Add(chartArea2);
+            this.moodChart.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            legend2.Name = "Legend1";
+            this.moodChart.Legends.Add(legend2);
+            this.moodChart.Location = new System.Drawing.Point(659, 335);
+            this.moodChart.Name = "moodChart";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.moodChart.Series.Add(series2);
+            this.moodChart.Size = new System.Drawing.Size(269, 185);
+            this.moodChart.TabIndex = 9;
+            this.moodChart.Text = "Mood Chart";
+            // 
+            // confidenceLabel
+            // 
+            this.confidenceLabel.AutoSize = true;
+            this.confidenceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confidenceLabel.Location = new System.Drawing.Point(792, 19);
+            this.confidenceLabel.Name = "confidenceLabel";
+            this.confidenceLabel.Size = new System.Drawing.Size(75, 13);
+            this.confidenceLabel.TabIndex = 10;
+            this.confidenceLabel.Text = "Confidence:";
+            // 
+            // supriseLabel
+            // 
+            this.supriseLabel.AutoSize = true;
+            this.supriseLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.supriseLabel.Location = new System.Drawing.Point(792, 146);
+            this.supriseLabel.Name = "supriseLabel";
+            this.supriseLabel.Size = new System.Drawing.Size(57, 13);
+            this.supriseLabel.TabIndex = 11;
+            this.supriseLabel.Text = "Surprise:";
+            // 
+            // angerLabel
+            // 
+            this.angerLabel.AutoSize = true;
+            this.angerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.angerLabel.Location = new System.Drawing.Point(792, 114);
+            this.angerLabel.Name = "angerLabel";
+            this.angerLabel.Size = new System.Drawing.Size(44, 13);
+            this.angerLabel.TabIndex = 12;
+            this.angerLabel.Text = "Anger:";
+            // 
+            // sorrowLabel
+            // 
+            this.sorrowLabel.AutoSize = true;
+            this.sorrowLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sorrowLabel.Location = new System.Drawing.Point(792, 84);
+            this.sorrowLabel.Name = "sorrowLabel";
+            this.sorrowLabel.Size = new System.Drawing.Size(50, 13);
+            this.sorrowLabel.TabIndex = 13;
+            this.sorrowLabel.Text = "Sorrow:";
+            // 
+            // joyLabel
+            // 
+            this.joyLabel.AutoSize = true;
+            this.joyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.joyLabel.Location = new System.Drawing.Point(792, 50);
+            this.joyLabel.Name = "joyLabel";
+            this.joyLabel.Size = new System.Drawing.Size(30, 13);
+            this.joyLabel.TabIndex = 14;
+            this.joyLabel.Text = "Joy:";
+            // 
+            // surpriseOutput
+            // 
+            this.surpriseOutput.Location = new System.Drawing.Point(855, 143);
+            this.surpriseOutput.Name = "surpriseOutput";
+            this.surpriseOutput.Size = new System.Drawing.Size(72, 20);
+            this.surpriseOutput.TabIndex = 15;
+            // 
+            // confidenceOutput
+            // 
+            this.confidenceOutput.Location = new System.Drawing.Point(873, 16);
+            this.confidenceOutput.Name = "confidenceOutput";
+            this.confidenceOutput.Size = new System.Drawing.Size(55, 20);
+            this.confidenceOutput.TabIndex = 16;
+            // 
+            // joyOutput
+            // 
+            this.joyOutput.Location = new System.Drawing.Point(828, 50);
+            this.joyOutput.Name = "joyOutput";
+            this.joyOutput.Size = new System.Drawing.Size(99, 20);
+            this.joyOutput.TabIndex = 17;
+            // 
+            // sorrowOutput
+            // 
+            this.sorrowOutput.Location = new System.Drawing.Point(848, 84);
+            this.sorrowOutput.Name = "sorrowOutput";
+            this.sorrowOutput.Size = new System.Drawing.Size(79, 20);
+            this.sorrowOutput.TabIndex = 18;
+            // 
+            // angerOutput
+            // 
+            this.angerOutput.Location = new System.Drawing.Point(842, 114);
+            this.angerOutput.Name = "angerOutput";
+            this.angerOutput.Size = new System.Drawing.Size(85, 20);
+            this.angerOutput.TabIndex = 19;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.SaveButton);
+            this.ClientSize = new System.Drawing.Size(940, 532);
+            this.Controls.Add(this.angerOutput);
+            this.Controls.Add(this.sorrowOutput);
+            this.Controls.Add(this.joyOutput);
+            this.Controls.Add(this.confidenceOutput);
+            this.Controls.Add(this.surpriseOutput);
+            this.Controls.Add(this.joyLabel);
+            this.Controls.Add(this.sorrowLabel);
+            this.Controls.Add(this.angerLabel);
+            this.Controls.Add(this.supriseLabel);
+            this.Controls.Add(this.confidenceLabel);
+            this.Controls.Add(this.moodChart);
+            this.Controls.Add(this.SaveAndSendButton);
             this.Controls.Add(this.SamplePictureBox);
             this.Controls.Add(this.recognizeButton);
             this.Controls.Add(this.TrainButton);
@@ -126,9 +254,10 @@
             this.Controls.Add(this.DetectButton);
             this.Controls.Add(this.picCapture);
             this.Name = "Form1";
-            this.Text = "Facial Recognition App";
+            this.Text = "moodi.io";
             ((System.ComponentModel.ISupportInitialize)(this.picCapture)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.SamplePictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.moodChart)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -143,7 +272,18 @@
         private System.Windows.Forms.Button TrainButton;
         private System.Windows.Forms.Button recognizeButton;
         private System.Windows.Forms.PictureBox SamplePictureBox;
-        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button SaveAndSendButton;
+        private System.Windows.Forms.DataVisualization.Charting.Chart moodChart;
+        private System.Windows.Forms.Label confidenceLabel;
+        private System.Windows.Forms.Label supriseLabel;
+        private System.Windows.Forms.Label angerLabel;
+        private System.Windows.Forms.Label sorrowLabel;
+        private System.Windows.Forms.Label joyLabel;
+        private System.Windows.Forms.TextBox surpriseOutput;
+        private System.Windows.Forms.TextBox confidenceOutput;
+        private System.Windows.Forms.TextBox joyOutput;
+        private System.Windows.Forms.TextBox sorrowOutput;
+        private System.Windows.Forms.TextBox angerOutput;
     }
 }
 
--- FacialRecognition/Form1.cs
+++ FacialRecognition/Form1.cs
@@ -16,6 +16,7 @@ using System.Threading;
 using System.Drawing.Text;
 using System.Diagnostics;
 using Google.Cloud.Vision.V1;
+using Newtonsoft.Json;
 
 namespace FacialRecognition
 {
@@ -28,6 +29,7 @@ namespace FacialRecognition
         private Image<Bgr, Byte> currentFrame = null; //Bgr is a color scheme, OpenCV uses BGR as their default. Here we are creating an image and defining that it will be in Bgr and represented as bytes
         private bool faceDetectionEnabled = false;
         //private string cascadeFileLocation = ""
+        public string currentPhotoPath = "";
 
         Image<Bgr, Byte> faceResult = null;
         List<Image<Gray, Byte>> TrainedFaces = new List<Image<Gray, byte>>();
@@ -35,9 +37,9 @@ namespace FacialRecognition
         List<string> PersonsNames = new List<string>();
         bool enableSaveImage = false;
         bool isTrained = false;
-        public string path = Directory.GetCurrentDirectory() + @"\Train_Images"; //Change direvtory for saving images here 
+        public string pathTrain = Directory.GetCurrentDirectory() + @"\Train_Images"; //Change direvtory for saving images here 
 
-        CascadeClassifier faceCascadeClassifier = new CascadeClassifier("C:\\Users\\wongr\\OneDrive\\Desktop\\CS_Projects\\FaceRecog\\FacialRecognition\\haarcascade_frontalface_default.xml"); //Declaring Capture Rectangle
+        CascadeClassifier faceCascadeClassifier = new CascadeClassifier("C:\\Users\\wongr\\OneDrive\\Desktop\\CS_Projects\\FaceRecog\\FacialRecognition\\haarcascade_frontalface_default.xml"); //Declaring Capture Rectangle; Static for now will add secret later
         Mat frame = new Mat();
         EigenFaceRecognizer recognizer;
 
@@ -93,29 +95,30 @@ namespace FacialRecognition
                         if (enableSaveImage)
                         {
 
-                            if (!Directory.Exists(path)) //We create this directory if it does not exist 
+                            if (!Directory.Exists(pathTrain)) //We create this directory if it does not exist 
                             {
-                                Directory.CreateDirectory(path);
+                                Directory.CreateDirectory(pathTrain);
                             }
-                            //Tasks are basically threads; Is this thread needed if we are only taking one picture?; Google Cloud API implementation starts here. Have it save image to directory then send to Google for classification
+                            ////Tasks are basically threads; Is this thread needed if we are only taking one picture?; Google Cloud API implementation starts here. Have it save image to directory then send to Google for classification
+                            Task.Factory.StartNew(() => {
+                                for (int i = 0; i < 10; i++)
+                                {
+                                    //resize the image then saving it
+                                    resultImage.Resize(200, 200, Inter.Cubic).Save(pathTrain + @"\" + textPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy-hh-mm-ss") + ".jpg");
+                                    Thread.Sleep(1000);
+                                }
+                            });
+                            ////TODO: Should I wipe this directory before making the sending out the request?
+                            //var client = ImageAnnotatorClient.Create();
 
-                            //TODO: Should I wipe this directory before making the sending out the request?
-                            var client = ImageAnnotatorClient.Create();
+                            //resultImage.Resize(200, 200, Inter.Cubic).Save(path + @"\" + textPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy") + ".jpg");
 
-                            resultImage.Resize(200, 200, Inter.Cubic).Save(path + @"\" + textPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy") + ".jpg");
+                            ////Load recently saved image into memory
+                            //var sentImage = Google.Cloud.Vision.V1.Image.FromFile(path + @"\" + textPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy") + ".jpg");
 
-                            //Load recently saved image into memory
-                            var sentImage = Google.Cloud.Vision.V1.Image.FromFile(path + @"\" + textPersonName.Text + "_" + DateTime.Now.ToString("dd-mm-yyyy") + ".jpg");
+                            ////Perform label detection on image file; here we are saving the JSON response
+                            //var response = client.DetectLabels(sentImage);
 
-                            //Perform label detection on image file; here we are saving the JSON response
-                            var response = client.DetectLabels(sentImage);
-                              
-                            //For Testing; Printing out all JSON classifications
-                            foreach (var annotation in response)
-                            {
-                                Console.WriteLine($"{annotation.Description}");
-                            }
-                            
                             enableSaveImage = false;
 
 
@@ -124,29 +127,32 @@ namespace FacialRecognition
                                 AddPersonButton.Invoke(new ThreadStart(delegate
                                 {
                                     AddPersonButton.Enabled = true;
-                                }));
+                                }));  
                             }
-
-                            //Step 1: Clear File Directory; Step 2: Take 10 file captures of face, Step 3: Send ONE image to Google for classification Step 4: Save the JSON results and display it in a graph. Step 5: TODO create a GUI to display most likely mood as well as results for all moods; Step 6: TODO Create a front end for entertainment results (????) 
 
 
                             //Step 5: Recognize Known Faces
                             if (isTrained)
                             {
                                 Image<Gray, Byte> grayFaceResult = resultImage.Convert<Gray, Byte>().Resize(200, 200, Inter.Cubic);
-
+                                CvInvoke.EqualizeHist(grayFaceResult, grayFaceResult);
                                 var result = recognizer.Predict(grayFaceResult);
-
+                                Debug.WriteLine(result.Label + ". " + result.Distance);
+                                
+                                //Here results found known faces
                                 if (result.Label != -1 && result.Distance < 2000)
                                 {
-                                    CvInvoke.PutText(currentFrame, textPersonName.Text, new Point(face.X - 2, face.Y - 2), FontFace.HersheyTriplex, 1.0, new Bgr(Color.Red).MCvScalar);
+                                    CvInvoke.PutText(currentFrame, PersonsNames[result.Label], new Point(face.X - 2, face.Y - 2),
+                                        FontFace.HersheyComplex, 1.0, new Bgr(Color.Blue).MCvScalar);
                                     CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Blue).MCvScalar, 3);
                                 }
-                                //If faces seen are not known; TODO Not recognizing for some reason
+                                //here results did not found any know faces
                                 else
                                 {
-                                    CvInvoke.PutText(currentFrame, "???", new Point(face.X - 2, face.Y - 2), FontFace.HersheyTriplex, 1.0, new Bgr(Color.Red).MCvScalar);
-                                    CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Blue).MCvScalar, 3);
+                                    CvInvoke.PutText(currentFrame, "????", new Point(face.X - 2, face.Y - 2),
+                                        FontFace.HersheyComplex, 1.0, new Bgr(Color.Red).MCvScalar);
+                                    CvInvoke.Rectangle(currentFrame, face, new Bgr(Color.Red).MCvScalar, 3);
+
                                 }
                             }
 
@@ -169,16 +175,32 @@ namespace FacialRecognition
 
         private void AddPersonButton_Click(object sender, EventArgs e)
         {
-            SaveButton.Enabled = true;
+            SaveAndSendButton.Enabled = true;
             AddPersonButton.Enabled = true;
             enableSaveImage = true;
         }
 
-        private void SaveButton_Click(object sender, EventArgs e)
+        private void SaveAndSendButton_Click(object sender, EventArgs e)
         { 
-            SaveButton.Enabled = false;
+            SaveAndSendButton.Enabled = false;
             AddPersonButton.Enabled = true;
             enableSaveImage = false;
+
+            var client = ImageAnnotatorClient.Create();
+            var image = Google.Cloud.Vision.V1.Image.FromFile("Sample Images\\happy.jfif"); //How to save image and send it to Google Cloud API; Create stats??
+            var response = client.DetectFaces(image).ToString(); //JSON response
+
+            //System.Diagnostics.Debug.Write(response.ToString());
+
+            dynamic dynJson = JsonConvert.DeserializeObject(response);
+
+            //Displayin JSON in respective fields
+            confidenceOutput.Text = $"Detection Confidence: {(dynJson[0]["detectionConfidence"].ToDouble() * 100).ToString()}%";
+            joyOutput.Text = $"Joy: {dynJson[0]["joyLikelihood"].ToString()}";
+            sorrowOutput.Text = $"Sorrow: {dynJson[0]["sorrowLikelihood"].ToString()}";
+            angerOutput.Text = $"Anger: {dynJson[0]["angerLikelihood"].ToString()}";
+            surpriseOutput.Text = $"Surpise: {dynJson[0]["surpriseLikelihood"].ToString()}";
+
         }
 
         private void TrainButton_Click(object sender, EventArgs e)
@@ -198,26 +220,42 @@ namespace FacialRecognition
 
             try
             {
-                string path = Directory.GetCurrentDirectory() + @"\Trained_Faces";
-                string[] files = Directory.GetFiles(path, "*.jpg", SearchOption.AllDirectories); //Creating array of all files
+                if (!Directory.Exists(Directory.GetCurrentDirectory() + @"\Trained_Faces")) //We create this directory if it does not exist 
+                {
+                    Directory.CreateDirectory(Directory.GetCurrentDirectory() + @"\Trained_Faces");
+                }
+
+                string pathTrained = Directory.GetCurrentDirectory() + @"\Trained_Faces";
+
+                string[] files = Directory.GetFiles(pathTrained, "*.jpg", SearchOption.AllDirectories); //Creating array of all files
 
                 foreach (var file in files)
                 {
                     Image<Gray, Byte> trainedImage = new Image<Gray, byte>(file);
+                    CvInvoke.EqualizeHist(trainedImage, trainedImage);
                     TrainedFaces.Add(trainedImage);
                     PersonsLabes.Add(imageCount);
                     string name = file.Split('\\').Last().Split('_')[0];
                     PersonsNames.Add(name);
                     imageCount++;
                     Debug.WriteLine(imageCount + ". " + name);
-                    imageCount++;
                 }
 
-                EigenFaceRecognizer recognizer = new EigenFaceRecognizer(imageCount, threshold);
-                recognizer.Train(TrainedFaces.ToArray(), PersonsLabes.ToArray());
-
-                isTrained = true;
-                return true;
+                if (TrainedFaces.Count() > 0)
+                {
+                    // recognizer = new EigenFaceRecognizer(ImagesCount,Threshold);
+                    recognizer = new EigenFaceRecognizer(imageCount, threshold);
+                    recognizer.Train(TrainedFaces.ToArray(), PersonsLabes.ToArray());
+                    isTrained = true;
+                    Debug.WriteLine(imageCount);
+                    //Debug.WriteLine(isTrained);
+                    return true;
+                }
+                else
+                {
+                    isTrained = false;
+                    return false;
+                }
 
             }
 
@@ -227,6 +265,8 @@ namespace FacialRecognition
                 MessageBox.Show("Error in Train Images: " + ex.Message);
                 return false;
             }
+
+            
         }
 
     }
